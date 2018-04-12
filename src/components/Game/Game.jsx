c930dff19e2c38c8143efad46c8fd03b439d4de6
DmitryBelonogy <32068247+DmitryBelonogy@users.noreply.github.com>
2018-04-12 16:19:54 +0300

    add

---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router,	Route,	Link, Redirect } from 'react-router-dom';
+// import Game from './components/Board';
+// import StartPage from './components/StartPage/StartPage';
+import './Game.css';
+
+const DATA = [
+  {
+  emogy: ['smile1', 'smile2','smile3','smile4'],
+  ans: 'asdfgh'
+  },
+  {
+  emogy: ['smile', 'smile','smile','smile'],
+  ans: 'qwerty'
+  },
+]
+
+class Game extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentData: DATA[0]
+    };
+  }
+
+  handleBtnClick = () => {
+    let input = document.querySelector('.inputAns').value;
+    if(input === this.state.currentData.ans) {
+      alert('good )))');
+      this.setState({
+        currentData: DATA[1],
+      })
+    } 
+  }
+
+  render() {
+    return(    
+      <div className='gamePage'>
+        <div className='emojy'>
+          {
+            this.state.currentData.emogy.map(item => <span key={item} className={`emogy ${item}`}>2</span>)
+          }
+        </div>
+        <input className='inputAns' type="text"/>
+        <button onClick={this.handleBtnClick}>Submit</button>
+      </div>   
+    )       
+  }
+}
+
+export default Game;
